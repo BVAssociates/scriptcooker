d3daa2abf5c75d8d97f4f2ea6864d95615cc5731
Vincent Bauchart <bauchart@bvassociates.fr>
2013-01-19 23:21:55 +0100

    Fix bad car

--- lib/ScriptCooker/Log.pm
+++ lib/ScriptCooker/Log.pm
@@ -13,7 +13,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with ScriptCooker.  If not, see <http://www.gnu.org/licenses/>.
 #
-﻿# ------------------------------------------------------------
+# ------------------------------------------------------------
 # Copyright (c) 2011, BV Associates. Tous droits reserves.
 # ------------------------------------------------------------
 #
